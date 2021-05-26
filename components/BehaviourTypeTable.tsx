4137c60e1da10365c55ee071ed1aef28e347b4ae
ElMayo <mic.l.meier@gmail.com>
2021-05-26 20:46:41 +0200

    feat: Add <BehaviourtypeTable/> component

---
+++ components/BehaviourTypeTable.tsx
@@ -0,0 +1,90 @@
+export default function BehaviourTypeTable() {
+  return (
+    <table>
+      <tr>
+        <th></th>
+        <th>Macher</th>
+        <th>Dynamiker</th>
+        <th>Beziehungsmensch</th>
+        <th>Analytiker</th>
+      </tr>
+      <tr>
+        <td>Verhalten unter Druck..</td>
+        <td>autokratisch</td>
+        <td>angreifend</td>
+        <td>nachgebend</td>
+        <td>ausweichend</td>
+      </tr>
+      <tr>
+        <td>Ist...</td>
+        <td>wirksam</td>
+        <td>anregend</td>
+        <td>angenehm</td>
+        <td>präzise</td>
+      </tr>
+      <tr>
+        <td>Strebt nach...</td>
+        <td>Macht</td>
+        <td>Anerkennung</td>
+        <td>Zustimmung</td>
+        <td>Respekt</td>
+      </tr>
+      <tr>
+        <td>Ist glücklich über...</td>
+        <td>Unabhängigkeit</td>
+        <td>Applaus</td>
+        <td>Konsens</td>
+        <td>Logik</td>
+      </tr>
+      <tr>
+        <td>Will gelobt werden für...</td>
+        <td>Zielerreichung</td>
+        <td>Bewunderung andrer</td>
+        <td>Atmosphäre</td>
+        <td>Detailtreue</td>
+      </tr>
+      <tr>
+        <td>Stärke...</td>
+        <td>Durchsetzung</td>
+        <td>Begeisterung</td>
+        <td>Unterstützung</td>
+        <td>Systematik</td>
+      </tr>
+      <tr>
+        <td>Hasst...</td>
+        <td>Unklarheit</td>
+        <td>Gleichförmigkeit</td>
+        <td>Konflikt</td>
+        <td>Unordnung</td>
+      </tr>
+      <tr>
+        <td>Benötigt...</td>
+        <td>Freiheit</td>
+        <td>Anreiz</td>
+        <td>Wärme</td>
+        <td>Klarheit</td>
+      </tr>
+      <tr>
+        <td>Spart gerne an...</td>
+        <td>Zeit</td>
+        <td>Analyse/Details</td>
+        <td>Kritik</td>
+        <td>Worte</td>
+      </tr>
+      <tr>
+        <td>Braucht zur Entscheidung...</td>
+        <td>Optionen</td>
+        <td>Vorbilder</td>
+        <td>Garantien</td>
+        <td>Beweise</td>
+      </tr>
+      <tr>
+        <td>Muss noch lernen...</td>
+        <td>zuhören</td>
+        <td>abwägen</td>
+        <td>initieren</td>
+        <td>entscheiden</td>
+      </tr>
+    </table>
+  )
+}
